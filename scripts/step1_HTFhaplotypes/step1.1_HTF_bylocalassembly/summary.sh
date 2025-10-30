168d4f4be8a2df3b3ff97e308269ba88e95a36f6
Jiajun Cui <48268391+JiajunCui-jjc@users.noreply.github.com>
2025-10-30 11:59:30 +0000

    Add files via upload

--- scripts/step1_HTFhaplotypes/step1.1_HTF_bylocalassembly/summary.sh
+++ scripts/step1_HTFhaplotypes/step1.1_HTF_bylocalassembly/summary.sh
@@ -1,15 +1,15 @@
 
 
-cd /SAN/ugi/plant_genom/jiajucui/4_mapping_to_pseudomonas/tailocin_2024_TF_Tapemeasure/2025_summer_paperfig_m57/results/step1_HTFhaplotypes/step1.1_HTF_bylocalassembly/historical46paf/mappings
+cd /SAN/ugi/plant_genom/jiajucui/4_mapping_to_pseudomonas/tailocin_2024_TF_Tapemeasure/2025_summer_paperfig_m57/results/step1_HTFhaplotypes/step1.1_HTF_bylocalassembly/historical49paf/mappings
 #cd ../../modern57paf/
 #cat top_matched_haplotypes_by_propcoverage_detailed.txt | cut -f1,2 > modern57_HTFhaplotype_bylocalassembly.txt
 
-#cat top_matched_haplotypes_by_propcoverage_detailed.txt | cut -f1,2 > h40_HTFhaplotype_bylocalassembly.txt
+cat top_matched_haplotypes_by_propcoverage_detailed.txt | cut -f1,2 > h49_HTFhaplotype_bylocalassembly.txt
 
 #cat h40_HTFhaplotype_bylocalassembly.txt ../../modern57paf/modern57_HTFhaplotype_bylocalassembly.txt > h40_m57_HTFhaplotype_bylocalassembly.txt
-tm1=h40_HTFhaplotype_bylocalassembly.txt
+tm1=h49_HTFhaplotype_bylocalassembly.txt
 tm2=../../modern57paf/modern57_HTFhaplotype_bylocalassembly.txt
-out=h40_m57_HTFhaplotype_bylocalassembly.txt
+out=h49_m57_HTFhaplotype_bylocalassembly.txt
 {
   # Print header (from the first file)
   head -n 1 "$tm1"
@@ -19,14 +19,14 @@ out=h40_m57_HTFhaplotype_bylocalassembly.txt
   tail -n +2 "$tm2"
 } | grep -v '^$' > "$out"
 
-input=h40_m57_HTFhaplotype_bylocalassembly.txt
-input2=../../tmp2h40_m57_HTFhaplotype_bylocalassembly.txt
+input=h49_m57_HTFhaplotype_bylocalassembly.txt
+input2=../../tmp2h49_m57_HTFhaplotype_bylocalassembly.txt
 
 awk 'BEGIN{OFS="\t"} 
 NR==1 {print $0} 
 NR>1  {$2="HTF_"$2; print}' "$input" > "$input2"
 
-output=../../final_h40_m57_HTFhaplotype_bylocalassembly.txt
+output=../../final_h49_m57_HTFhaplotype_bylocalassembly.txt
 
 awk 'BEGIN {
   OFS = "\t"
