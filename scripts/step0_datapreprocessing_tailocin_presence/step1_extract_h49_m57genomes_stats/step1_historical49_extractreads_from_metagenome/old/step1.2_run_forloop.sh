78a803c059cffa45cbef16920dedf858be17bf77
Jiajun Cui <48268391+JiajunCui-jjc@users.noreply.github.com>
2025-11-07 16:34:00 +0000

    Delete scripts/step0_datapreprocessing_tailocin_presence/step1_extract_h49_m57genomes_stats/step1_historical49_extractreads_from_metagenome/old directory

--- scripts/step0_datapreprocessing_tailocin_presence/step1_extract_h49_m57genomes_stats/step1_historical49_extractreads_from_metagenome/old/step1.2_run_forloop.sh
+++
@@ -1,8 +0,0 @@
-echo 'samplename At_percent At_covered At_depth Ps_percent Ps_covered Ps_depth'  >> 2024_162_4th_answers_forplot.txt 
-
-
-for line in $(cat samplenames.txt | sed -n $i'p'| awk '{print $1}');
-do
-bash plotdatagenerate.sh $line >> summarydata.txt;
-done
-
--- scripts/step0_datapreprocessing_tailocin_presence/step1_extract_h49_m57genomes_stats/step1_historical49_extractreads_from_metagenome/old/tmp_rm_beforererun.sh
+++
@@ -1,6 +0,0 @@
-rm /SAN/ugi/plant_genom/jiajucui/answer2025_h49/*
-rm /SAN/ugi/plant_genom/jiajucui/4_mapping_to_A_thaliana/2025_h49/*
-rm /SAN/ugi/plant_genom/jiajucui/4_mapping_to_pseudomonas/2025_h49/*
-rm /SAN/ugi/plant_genom/jiajucui/4_mapping_to_pseudomonas/tailocin_2024_TF_Tapemeasure/2025_summer_paperfig_m57/data/all_fastq_h49/At_bams_h49_dedup/*
-rm /SAN/ugi/plant_genom/jiajucui/4_mapping_to_pseudomonas/tailocin_2024_TF_Tapemeasure/2025_summer_paperfig_m57/data/all_fastq_h49/Ps_bams_h49_dedup/*
-
